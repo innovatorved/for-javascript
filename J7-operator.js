efcee16dff4876cc7a6e2d12ac540ed87d81772f
Next In <nextin.520@gmail.com>
2021-07-04 10:55:11 +0000

    operators

---
+++ J7-operator.js
@@ -0,0 +1,77 @@
+/*
+JavaScript Arithmetic Operators
+
+Arithmetic operators are used to perform arithmetic on numbers:
+Operator 	Description
++ 	Addition
+- 	Subtraction
+* 	Multiplication
+** 	Exponentiation (ES2016)
+/ 	Division
+% 	Modulus (Division Remainder)
+++ 	Increment
+-- 	Decrement
+
+
+
+JavaScript Assignment Operators
+
+Assignment operators assign values to JavaScript variables.
+Operator 	Example 	Same As
+= 	x = y 	x = y
++= 	x += y 	x = x + y
+-= 	x -= y 	x = x - y
+*= 	x *= y 	x = x * y
+/= 	x /= y 	x = x / y
+%= 	x %= y 	x = x % y
+**= 	x **= y 	x = x ** y
+The addition assignment operator (+=) adds a value to a variable.
+
+
+JavaScript Comparison Operators
+Operator 	Description
+== 	equal to
+=== 	equal value and equal type
+!= 	not equal
+!== 	not equal value or not equal type
+> 	greater than
+< 	less than
+>= 	greater than or equal to
+<= 	less than or equal to
+? 	ternary operator
+
+
+
+Comparison operators are fully described in the JS Comparisons chapter.
+JavaScript Logical Operators
+Operator 	Description
+&& 	logical and
+|| 	logical or
+! 	logical not
+
+
+
+Logical operators are fully described in the JS Comparisons chapter.
+JavaScript Type Operators
+Operator 	Description
+typeof 	Returns the type of a variable
+instanceof 	Returns true if an object is an instance of an object type
+
+Type operators are fully described in the JS Type Conversion chapter.
+JavaScript Bitwise Operators
+
+Bit operators work on 32 bits numbers.
+Any numeric operand in the operation is converted into a 32 bit number. The result is converted back to a JavaScript number.
+Operator 	Description 	Example 	Same as 	Result 	Decimal
+& 	AND 	5 & 1 	0101 & 0001 	0001 	 1
+| 	OR 	5 | 1 	0101 | 0001 	0101 	 5
+~ 	NOT 	~ 5 	 ~0101 	1010 	 10
+^ 	XOR 	5 ^ 1 	0101 ^ 0001 	0100 	 4
+<< 	Zero fill left shift 	5 << 1 	0101 << 1 	1010 	 10
+>> 	Signed right shift 	5 >> 1 	0101 >> 1 	0010 	  2
+>>> 	Zero fill right shift 	5 >>> 1 	0101 >>> 1 	0010 	  2
+
+
+
+*/
+
