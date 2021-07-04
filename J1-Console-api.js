ed6e72b7fedc7c5ce7ad8771ba54c6a89460862a
Next In <nextin.520@gmail.com>
2021-07-04 08:40:36 +0000

    Connsole Api

---
+++ J1-Console-api.js
@@ -0,0 +1,21 @@
+/*
+    console Api 
+    - to write msg on console
+*/
+
+// write msg on Console
+console.log("Hello i am message");  
+console.log("1+1 : " , 1+1);  
+
+// write warning msg in console
+console.warn("Warning");  
+
+// make error in console
+console.error("Write error msg"); 
+
+// Clear Console
+console.clear()
+
+// if false make an error
+console.assert(1 == "1");  // true
+console.assert(10 == (20-5-5+1)); // False
